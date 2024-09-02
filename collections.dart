e884db0a242ad3d4f0b9510547401b2dfc85a10a
IsrakAhmed <israkahmed7@gmail.com>
2024-09-02 22:09:30 +0600

    list done

---
+++ collections.dart
@@ -0,0 +1,17 @@
+// List
+
+main() {
+  List names_and_ages = ['israk', 'riya', '25', '24'];
+
+  for (var n in names_and_ages) {
+    //print(n);
+  }
+
+  List<String> last_names = ['ahmed', 'jahan'];
+
+  print(last_names.indexOf('jahan'));
+
+  for (var n in last_names) {
+    //print(n);
+  }
+}
