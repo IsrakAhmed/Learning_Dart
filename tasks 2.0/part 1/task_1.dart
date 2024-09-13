813ebf405699a33f698c70913a3aac67e452acb7
IsrakAhmed <israkahmed7@gmail.com>
2024-09-13 19:27:01 +0600

    problem statements added into the files

--- tasks 2.0/part 1/task_1.dart
+++ tasks 2.0/part 1/task_1.dart
@@ -1,3 +1,26 @@
+/*
+
+    < Task 1. Built-in Types >
+    
+    ●   Task:   Write a Dart program that demonstrates the usage of various built-in types such
+                as int, double, String, bool, and List. Create variables of each type and perform
+                basic operations on them.
+
+        Example:
+
+            ○ Create an integer variable to represent age.
+
+            ○ Create a double variable to represent height.
+
+            ○ Create a string variable to represent a person’s name.
+
+            ○ Create a boolean variable to represent if the person is a student.
+            
+            ○ Create a list to store the person’s grades.
+
+*/
+
+
 import 'dart:io';
 
 main () {
--- tasks 2.0/part 1/task_2.dart
+++ tasks 2.0/part 1/task_2.dart
@@ -1,3 +1,19 @@
+/*
+
+    < Task 2. Records >
+    
+    ●   Task:   Implement a Dart record to group related data together. Create a record for a
+                Book that includes the title, author, and number of pages. Write a function to
+                display the record's contents.
+
+        Example:
+
+            ○ Define a record (String title, String author, int pages) for a book.
+
+            ○ Use pattern matching to extract and display the book’s details.
+
+*/
+
 
 main () {
 
